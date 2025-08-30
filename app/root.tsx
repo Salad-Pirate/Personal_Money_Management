ba45ff686678bdc54e92f6b6de8afe887faae739
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2025-08-30 15:20:49 +0700

    PUSH

---
+++ app/root.tsx
@@ -0,0 +1,43 @@
+import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
+
+import type { Route } from "./+types/root";
+import "./app.css";
+
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <h1>Hello</h1>;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+
+  return (
+    <main className="pt-16 p-4 container mx-auto">
+        
+    </main>
+  );
+}
