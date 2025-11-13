6203d4fc74e354b10ca4ce2893431ee0607002b3
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2025-11-13 13:29:26 +0700

    Update next.config.mjs
    Add:
    - Clear Default path

--- pmm_app/next.config.mjs
+++ pmm_app/next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   // เปลี่ยนชื่อ option ตาม Next.js 15
   serverExternalPackages: ['tesseract.js'],
 
-  basePath: '/salad-pirate-frontend',
-  assetPrefix: '/salad-pirate-frontend/',
-  output: 'standalone',
-  reactStrictMode: true,
+
   outputFileTracingIncludes: {
     '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.proto']
   },
