47a0f800802adbca7e847cffdc6df8c2bcd9500d
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2025-11-21 01:21:56 +0700

    Update next.config.mjs

--- pmm_app/next.config.mjs
+++ pmm_app/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   // เปลี่ยนชื่อ option ตาม Next.js 15
   serverExternalPackages: ['tesseract.js'],
-
+  basePath: '/salad-pirate-frontend',
   outputFileTracingIncludes: {
     '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.proto']
   },
